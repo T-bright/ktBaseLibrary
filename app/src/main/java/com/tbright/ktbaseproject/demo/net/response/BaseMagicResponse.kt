306a4e81153b0161b192d89f832fc03285f26e8a
T-bright <t_bright@163.com>
2020-06-15 16:11:13 +0800

    pref ：优化demo代码

--- app/src/main/java/com/tbright/ktbaseproject/demo/net/response/BaseMagicResponse.kt
+++ app/src/main/java/com/tbright/ktbaseproject/demo/net/response/BaseMagicResponse.kt
@@ -1,7 +1,6 @@
 package com.tbright.ktbaseproject.demo.net.response
 
 import com.tbright.ktbaselibrary.base.BaseResponse
-import com.tbright.ktbaseproject.demo.customconfig.RESPONSE_OK
 
 class BaseMagicResponse<T> : BaseResponse<T>() {
 
